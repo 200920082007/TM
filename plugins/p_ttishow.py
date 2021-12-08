cfe7cb637e1c06b20944e45c1c1bf890c80b366e
Rakesh Meena <82395901+rakeshyt@users.noreply.github.com>
2021-12-08 07:27:04 +0530

    Update p_ttishow.py

--- plugins/p_ttishow.py
+++ plugins/p_ttishow.py
@@ -240,7 +240,10 @@ async def list_users(bot, message):
     users = await db.get_all_users()
     out = "Users Saved In DB Are:\n\n"
     async for user in users:
-        out += f"<a href=tg://user?id={user['id']}>{user['name']}</a>\n"
+        out += f"<a href=tg://user?id={user['id']}>{user['name']}</a>"
+        if user['ban_status']['is_banned']:
+            out += '( Banned User )'
+        out += '\n'
     try:
         await raju.edit_text(out)
     except MessageTooLong:
