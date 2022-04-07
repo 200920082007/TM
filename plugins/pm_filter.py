4e5f2e6fcba67257c3827830e702ade785df3f90
Techno Mindz <97782515+TechnoMindz@users.noreply.github.com>
2022-04-07 21:37:38 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -378,7 +378,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
                     file_id=file_id,
                     caption=f_caption
                     )
-                await query.answer('Check PM, I Have Sent Files In PM',show_alert = True)
+                await query.answer('Check PM, I Have Sent Files There 😉',show_alert = True)
         except UserIsBlocked:
             await query.answer('Unblock the bot mahn !',show_alert = True)
         except PeerIdInvalid:
